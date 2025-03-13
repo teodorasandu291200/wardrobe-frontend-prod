02bf03a2ff9cd4851a7dc2a82687b77f201f89cf
Teodora Sandu <sandu.teodora2912@gmail.com>
2025-03-13 23:25:08 +0000

    Solved dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
+// filepath: /c:/Users/sandu/Virtuwear-PROD/frontend/postcss.config.js
 module.exports = {
   plugins: [
-    require('tailwindcss'),
+    require('@tailwindcss/postcss7-compat'),
     require('autoprefixer'),
+    require('postcss-flexbugs-fixes'),
   ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
-// tailwind.config.js
+// filepath: /c:/Users/sandu/Virtuwear-PROD/frontend/tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      "./src/**/*.{js,jsx,ts,tsx}", // Adjust according to your file extensions
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
-      extend: {
-          fontFamily: {
-              italiana: ["Italiana", "Helvetica", "sans-serif"],
-              nunito: ['Nunito Sans', 'Helvetica', 'sans-serif'],
-            },
+    extend: {
+      fontFamily: {
+        italiana: ["Italiana", "Helvetica", "sans-serif"],
+        nunito: ['Nunito Sans', 'Helvetica', 'sans-serif'],
       },
+    },
   },
   plugins: [],
 };
